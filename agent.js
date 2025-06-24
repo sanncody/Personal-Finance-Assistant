1f5e5d54d1dd9629a923fa65afd8ff8bf92a3d94
Sandeep <sandeepwadhawan2001@gmail.com>
2025-06-25 03:00:07 +0530

    Initiate LLM setup

--- agent.js
+++ agent.js
@@ -1,1 +1,21 @@
-console.log(process.env.GROQ_API_KEY);
+import Groq from 'groq-sdk';
+
+const groq = new Groq({
+    apiKey: process.env.GROQ_API_KEY
+});
+
+const callAgent = async () => {
+    const completion = await groq.chat.completions.create({
+        messages: [
+            {
+                role: "user",
+                content: "hi",
+            },
+        ],
+        model: "llama-3.3-70b-versatile"
+    });
+
+    console.log(completion);
+};
+
+callAgent();
