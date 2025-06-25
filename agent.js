9aa3b0404151210b3b5ff57cc3eead2c6fd9126e
Sandeep <sandeepwadhawan2001@gmail.com>
2025-06-26 02:19:33 +0530

    add system prompt

--- agent.js
+++ agent.js
@@ -8,14 +8,18 @@ const callAgent = async () => {
     const completion = await groq.chat.completions.create({
         messages: [
             {
-                role: "user",
-                content: "hi",
+                role: "system",
+                content: "You are Personal finance Assistant. Your task is to assist users with their expenses, balances, and financial planning."
+            },
+            {
+                role: "user", //Role: User means end-user message
+                content: "How much money I have spent this month?",
             },
         ],
         model: "llama-3.3-70b-versatile"
     });
 
-    console.log(completion);
+    console.log(completion.choices[0].message);
 };
 
 callAgent();
