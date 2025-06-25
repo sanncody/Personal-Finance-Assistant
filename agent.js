3ff3b00765c99e431521e16a6aafd4f914512629
Sandeep <sandeepwadhawan2001@gmail.com>
2025-06-26 03:06:57 +0530

    add getTotalExpense tool

--- agent.js
+++ agent.js
@@ -9,17 +9,113 @@ const callAgent = async () => {
         messages: [
             {
                 role: "system",
-                content: "You are Personal finance Assistant. Your task is to assist users with their expenses, balances, and financial planning."
+                content: `You are Personal finance Assistant. Your task is to assist users with their expenses, balances, and financial planning.
+                current DateTime: ${new Date().toUTCString()}`
             },
             {
                 role: "user", //Role: User means end-user message
                 content: "How much money I have spent this month?",
             },
         ],
-        model: "llama-3.3-70b-versatile"
+        model: "llama-3.3-70b-versatile",
+        tools: [
+            {
+                type: 'function',
+                function: {
+                    name: 'getTotalExpense',
+                    description: "It returns us total expense from between from and to date.",
+                    parameters:{
+                        type: "object",
+                        properties: {
+                            from: {
+                                type: "string",
+                                description: "From date to get the expense."
+                            },
+                            to: {
+                                type: "string",
+                                description: "To date to get the expense."
+                            }
+                        }
+                    } 
+                }
+            }
+        ],
     });
 
-    console.log(completion.choices[0].message);
+    console.log(JSON.stringify(completion.choices[0], null, 2));
+
+    const toolCalls = completion.choices[0].message.tool_calls;
+
+    // If there is no tool calling that means we got the response.
+    if (!toolCalls) {
+        console.log(`Assistant: ${completion.choices[0].message.content}`);
+        return;
+    }
+
+    for (let tool of toolCalls) {
+        const functionName = tool.function.name;
+        const functionArgs = tool.function.arguments;
+
+        let result = "";
+        if (functionName === 'getTotalExpense') {
+            result = getTotalExpense(JSON.parse(functionArgs)).toString();
+        }
+        
+        const completionAgain = await groq.chat.completions.create({
+            messages: [
+                {
+                    role: "system",
+                    content: `You are Personal finance Assistant. Your task is to assist users with their expenses, balances, and financial planning.
+                current DateTime: ${new Date().toUTCString()}`
+                },
+                {
+                    role: "user", //Role: User means end-user message
+                    content: "How much money I have spent this month?",
+                },
+                {
+                    role: "tool",
+                    content: result,
+                    tool_call_id: tool.id
+                }
+            ],
+            model: "llama-3.3-70b-versatile",
+            tools: [
+                {
+                    type: 'function',
+                    function: {
+                        name: 'getTotalExpense',
+                        description: "It returns us total expense from between from and to date.",
+                        parameters: {
+                            type: "object",
+                            properties: {
+                                from: {
+                                    type: "string",
+                                    description: "From date to get the expense."
+                                },
+                                to: {
+                                    type: "string",
+                                    description: "To date to get the expense."
+                                }
+                            }
+                        }
+                    }
+                }
+            ],
+        });
+
+        console.log(completionAgain.choices[0]);
+    }
 };
 
 callAgent();
+
+/**
+ * Get total expense
+ */
+
+function getTotalExpense({ from, to }) {
+    console.log("Calling getTotalExpense tool...");
+
+    // In reality, we call DB here...
+    return 20000;
+}
